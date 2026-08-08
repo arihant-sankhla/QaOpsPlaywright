8747e440c72d98a7a7ad8accf299f6af29b41bc9
Arihant Sankhla <arihantsankhla11@gmail.com>
2026-08-08 23:16:03 +0530

    Code Update

--- tests/webClientAppLogin.spec.js
+++ tests/webClientAppLogin.spec.js
@@ -3,7 +3,7 @@ const { test, expect } = require('@playwright/test');
  
  
  
-test.only('@Webst Client App login', async ({ page }) => {
+test.skip('@Webst Client App login', async ({ page }) => {
    //js file- Login js, DashboardPage
    const email = "anshika@gmail.com";
    const productName = 'ZARA COAT 3';
